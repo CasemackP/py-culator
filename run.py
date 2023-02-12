935f97885d2f477b4d240843e52aad4b1a953705
Casey Poppleton <casey.poppleton@gmail.com>
2023-02-11 17:54:18 -0800

    Define main function

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
-print("Hello World! :)")
 
-print("hello world the second")
+
 
 def subtract(a, b):
     return a - b
+
+def main():
+    print("Hello World! :)")
+    print("hello world the second")
+
+main()
